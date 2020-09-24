067d671abf3d55a5ce2854f83b90d2c8c8a179c3
Johan Gustafsson <gustajo@chalmers.se>
2020-09-24 11:52:27 +0200

    Moved code from HMA Sandbox to this repo

---
+++ PythonGui/MoClo.py
@@ -0,0 +1,328 @@
+import tkinter as tk
+from tkinter import ttk
+import tkinter.font as tkFont
+import primer3 as p3
+import tkinter.filedialog as tkFile
+import os
+
+
+#print(p3.calcTm('GTAAAACGACGGCCAGT'))
+
+
+
+def reverse(s):
+    return s[::-1]
+
+def compChar(s):
+    compSwitcher = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C",
+    }
+    return compSwitcher.get(s, "X")
+
+print(compChar("A"))
+
+def cleanupSeq(s):
+    ret = ""
+    for i in range(len(s)):
+        if (s[i] == 'A') | (s[i] == 'T') | (s[i] == 'C') | (s[i] == 'G'):
+            ret += s[i]
+    return ret
+
+def complementary(s):
+    ret = ""
+    for i in range(len(s)):
+        ret += compChar(s[i])
+    return ret
+
+def gcContent(s):
+    gcs = 0
+    for i in range(len(s)):
+        if (s[i] == 'G') | (s[i] == 'C'):
+            gcs += 1
+    return gcs/len(s)
+
+#test
+print(gcContent("AATTGGCCAATTGGCC"))
+
+
+def meltTemp(s):
+    return (p3.calcTm(s))
+def hairpin(s):
+    return (p3.calcHairpin(s))
+
+#print(hairpin('AATTTCCCCGGGG'))
+
+def checkPrimer(s):
+    gcCont = gcContent(s)
+    #todo: add borders for gcContent?
+    mt = meltTemp(s)
+    if (mt < 48) | (mt > 60):
+        return False
+    return True
+print(complementary("ATTCG"))
+print(gcContent("ATTCG"))
+
+#for forward primer
+univStartTag = "GCATCGTCTCATCGGTCTCA"
+startTags = dict()
+#startTags["Type 1"] = "CCCT"
+startTags["Type 2"] = "AACG"
+startTags["Type 3"] = "TATG"
+startTags["Type 3A"] = "TATG"
+startTags["Type 3B"] = "TTCT"
+startTags["Type 4"] = "ATCCTAACTCGAG"
+startTags["Type 4A"] = "ATCC"
+startTags["Type 4B"] = "TGGC"
+#startTags["Type 5"] = "GCTG"
+startTags["Type 6"] = "TACA"
+startTags["Type 7"] = "GAGT"
+startTags["Type 8"] = "CCGAGCGGCCGC"
+startTags["Type 8A"] = "CCGAGCGGCCGC"
+startTags["Type 8B"] = "CAAT"
+
+univEndTag = "TGAGACCTGAGACGGCAT"
+
+endTags = dict()
+#endTags["Type 1"] = "AACG"
+endTags["Type 2"] = "AGATCTATG"
+endTags["Type 3"] = "GGATCC"
+endTags["Type 3A"] = "GGTTCT"
+endTags["Type 3B"] = "GGATCC"
+endTags["Type 4"] = "GCTG"
+endTags["Type 4A"] = "TAACTCGAGTGGC"
+endTags["Type 4B"] = "GCTG"
+#endTags["Type 5"] = "TACA"
+endTags["Type 6"] = "GAGT"
+endTags["Type 7"] = "CCGA"
+endTags["Type 8"] = "GCGGCCGCCCCT"
+endTags["Type 8A"] = "GCGGCCGCCAAT"
+endTags["Type 8B"] = "CCCT"
+
+
+enzymeRecSitesIds = ["BsaI", "BsmBI", "NotI", "BglII", "BamHI", "XhoI"]
+enzymeRecSitesSeqs = ["GGTCTC", "CGTCTC", "GCGGCCGC", "AGATCT", "GGATCC", "CTCGAG"]
+
+
+
+root = tk.Tk()
+# s = ttk.Style()
+# s.theme_use('clam')
+
+root.title('MoClo Primer Design Tool')
+root.minsize(400, 300)
+inp = tk.Text(root, width=140, height=10)
+inp.focus()
+
+outpFwd = tk.Text(root, width=140, height=12)
+outpFwd.config(state='disabled')
+
+outpRev = tk.Text(root, width=140, height=12)
+outpRev.config(state='disabled')
+
+#make font a bit larger
+fontStyle = tkFont.Font(family="Arial", size=14)
+
+filterVar = tk.IntVar(value=1)
+filterChk = tk.Checkbutton(root, text="Filter primers", variable=filterVar, height=3, font=fontStyle)
+
+warnings = tk.Text(root, width=70, height=8, foreground="red")
+warnings.config(state='disabled')
+
+
+def onGenButton():
+    #expButton.config(state='normal') not needed
+    seq = cleanupSeq(inp.get("1.0", tk.END))
+    # loop through all primer lengths
+#    forwardPrimers = list()
+#    reversePrimers = list()
+    startId = startCombo.get()
+    startTag = startTags[startId]
+    endId = endCombo.get()
+    endTag = endTags[endId]
+    fwInd = 1
+    revInd = 1
+
+    outpFwd.config(state='normal')
+    outpFwd.delete("1.0", tk.END)
+    outpRev.config(state='normal')
+    outpRev.delete("1.0", tk.END)
+
+    global fwdPrimers
+    global revPrimers
+    global fwdPrimersText
+    global revPrimersText
+    fwdPrimers = list()
+    revPrimers = list()
+    fwdPrimersText = list()
+    revPrimersText = list()
+
+
+
+    for length in range(17, 29):
+        forwardPrimer = seq[0:length]
+        if (filterVar.get() == 0) | checkPrimer(forwardPrimer):
+            fwdPrimers.append(fwdPrimers)
+            outputString = univStartTag + startTag + forwardPrimer + '\tGC cont: ' + '{:d}'.format(round(gcContent(forwardPrimer)*100))+ '%' + '\tLength: ' + str(len(forwardPrimer)) + '\tMelt temp: ' + '{:.01f}'.format(meltTemp(forwardPrimer)) + ' Hairpin Tm: {:.01f}'.format(hairpin(forwardPrimer).tm) + '\n'
+            fwdPrimersText.append(outputString)
+            outpFwd.insert(str(fwInd) + ".0", outputString)
+            fwInd += 1
+
+        reversePrimer = reverse(seq[-length::]) #reverses automatically
+        if (filterVar.get() == 0) | checkPrimer(reversePrimer):
+            revPrimers.append(reversePrimer)
+            outputString = complementary(reverse(univEndTag)) + endTag + complementary(reversePrimer) + '\tGC cont: ' + '{:d}'.format(round(gcContent(reversePrimer)*100))+ '%' + '\tLength: ' + str(len(reversePrimer)) + '\tMelt temp: ' + '{:.01f}'.format(meltTemp(reversePrimer)) + ' Hairpin Tm: {:.01f}'.format(hairpin(reversePrimer).tm)  + '\n'
+            revPrimersText.append(outputString)
+            outpRev.insert(str(revInd) + ".0", outputString)
+            revInd += 1
+    #remove the last newline
+    outpFwd.delete(str(fwInd) + ".0", tk.END)
+    outpRev.delete(str(revInd) + ".0", tk.END)
+    outpFwd.config(state='disabled')
+    outpRev.config(state='disabled')
+
+    #add warnings:
+    #enzyme recognition sites:
+    warnings.config(state='normal')
+    warnings.delete("1.0", tk.END)
+
+    warnInd = 1
+
+    for i in range(len(enzymeRecSitesIds)):
+        a = -1
+        while True:
+            a = seq.find(enzymeRecSitesSeqs[i], a+1)
+            if a == -1:
+                break
+            warnings.insert(str(warnInd) + ".0", "Restr. enzyme recogn. site found in seq: " + enzymeRecSitesIds[i] + ": " + enzymeRecSitesSeqs[i] + " at pos " + str(a) + "\n")
+            warnInd += 1
+    #issue warnings for certain parts, since the start/end codon is included in the primer
+    if startId == "Type 3" | startId == "Type 3A":
+        warnings.insert(str(warnInd) + ".0",
+                        "Start codon is already included in fwd primer. Do not include stop codon in pasted sequence if part will be used as fusion or tagged protein.\n")
+        warnInd += 1
+    if endId == "Type 3B":
+        warnings.insert(str(warnInd) + ".0",
+                        "Stop codon is already included in rev primer. Do not include stop codon in pasted sequence if part will be used as fusion or tagged protein.\n")
+        warnInd += 1
+
+    warnings.delete(str(warnInd) + ".0", tk.END)
+    warnings.config(state='disabled')
+
+#win = None #just assign to something
+
+
+def onExpButton():
+    #global win
+    #win = tk.Toplevel()
+
+    #win.wm_title("Window")
+    #win.grab_set()
+    #lb = tk.Listbox(win)
+    #lb.pack
+    #but = tk.Button(win, command=onFinishExp)
+    #but.pack();
+
+
+    filename = tkFile.asksaveasfilename(initialdir=os.getcwd(), title="Select file", defaultextension='.FASTA', filetypes=(("fasta files","*.FASTA"),("all files","*.*")))
+    print(filename)
+
+    fwdOverhangStd = "TCGGTCTCA"
+    fwdOverhangTypeSpec = startTags[startCombo.get()]
+    seq = cleanupSeq(inp.get("1.0", tk.END))
+    revOverhangStd = "TGAGACC"
+    revOverhangTypeSpec = endTags[endCombo.get()]
+    backbone = "AGACCAATAAAAAACGCCCGGCGGCAACCGAGCGTTCTGAACAAATCCAGATGGAGTTCTGAGGTCATTACTGGATCTATCAACAGGAGTCCAAGCGAGCTCGATATCAAATTACGCCCCGCCCTGCCACTCATCGCAGTACTGTTGTAATTCATTAAGCATTCTGCCGACATGGAAGCCATCACAAACGGCATGATGAACCTGAATCGCCAGCGGCATCAGCACCTTGTCGCCTTGCGTATAATATTTGCCCATGGTGAAAACGGGGGCGAAGAAGTTGTCCATATTGGCCACGTTTAAATCAAAACTGGTGAAACTCACCCAGGGATTGGCTGAAACGAAAAACATATTCTCAATAAACCCTTTAGGGAAATAGGCCAGGTTTTCACCGTAACACGCCACATCTTGCGAATATATGTGTAGAAACTGCCGGAAATCGTCGTGGTATTCACTCCAGAGCGATGAAAACGTTTCAGTTTGCTCATGGAAAACGGTGTAACAAGGGTGAACACTATCCCATATCACCAGCTCACCGTCTTTCATTGCCATACGAAATTCCGGATGAGCATTCATCAGGCGGGCAAGAATGTGAATAAAGGCCGGATAAAACTTGTGCTTATTTTTCTTTACGGTCTTTAAAAAGGCCGTAATATCCAGCTGAACGGTCTGGTTATAGGTACATTGAGCAACTGACTGAAATGCCTCAAAATGTTCTTTACGATGCCATTGGGATATATCAACGGTGGTATATCCAGTGATTTTTTTCTCCATTTTAGCTTCCTTAGCTCCTGAAAATCTCGATAACTCAAAAAATACGCCCGGTAGTGATCTTATTTCATTATGGTGAAAGTTGGAACCTCTTACGTGCCCGATCAATCATGACCAAAATCCCTTAACGTGAGTTTTCGTTCCACTGAGCGTCAGACCCCGTAGAAAAGATCAAAGGATCTTCTTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCTTGCAAACAAAAAAACCACCGCTACCAGCGGTGGTTTGTTTGCCGGATCAAGAGCTACCAACTCTTTTTCCGAAGGTAACTGGCTTCAGCAGAGCGCAGATACCAAATACTGTTCTTCTAGTGTAGCCGTAGTTAGGCCACCACTTCAAGAACTCTGTAGCACCGCCTACATACCTCGCTCTGCTAATCCTGTTACCAGTGGCTGCTGCCAGTGGCGATAAGTCGTGTCTTACCGGGTTGGACTCAAGACGATAGTTACCGGATAAGGCGCAGCGGTCGGGCTGAACGGGGGGTTCGTGCACACAGCCCAGCTTGGAGCGAACGACCTACACCGAACTGAGATACCTACAGCGTGAGCTATGAGAAAGCGCCACGCTTCCCGAAGGGAGAAAGGCGGACAGGTATCCGGTAAGCGGCAGGGTCGGAACAGGAGAGCGCACGAGGGAGCTTCCAGGGGGAAACGCCTGGTATCTTTATAGTCCTGTCGGGTTTCGCCACCTCTGACTTGAGCGTCGATTTTTGTGATGCTCGTCAGGGGGGCGGAGCCTATGGAAAAACGCCAGCAACGCGGCCTTTTTACGGTTCCTGGCCTTTTGCTGGCCTTTTGCTCACATGTTCTTTCCTGCGTTATCCCCTGATTCTGTGGATAACCGTAG"
+
+    totSeqToExp = fwdOverhangStd + fwdOverhangTypeSpec + seq + revOverhangTypeSpec + revOverhangStd + backbone
+
+    #now write the FASTA file:
+    file1 = open(filename, "w")
+    file1.write(">Yeast toolkit export. Starts at " + startCombo.get() + ", ends at " + endCombo.get() + "\n")
+    ll = len(totSeqToExp)
+    for i in range(0, ll, 80):
+        file1.write(totSeqToExp[i:min(i+80, ll)] + '\n')
+
+    file1.close()
+
+
+#def onFinishExp():
+#    win.destroy()
+
+
+buttonFrame = tk.Frame(root)
+
+genButton = tk.Button(buttonFrame,
+                   command=onGenButton,
+                   text="Generate\nprimers",
+                   width=13,
+                   height=2,
+                   font=fontStyle
+)
+
+expButton = tk.Button(buttonFrame,
+                   command=onExpButton,
+                   text="Export\nFASTA",
+                   width=13,
+                   height=2,
+                   font=fontStyle
+)
+#expButton.config(state='disabled') #not needed
+
+#test = tk.Listbox(root)
+#test.insert(1, "Nachos")
+#test.insert(2, "Sandwich")
+#test.insert(3, "Burger")
+#test.insert(4, "Pizza")
+#test.insert(5, "Burrito")
+
+
+startCombo = ttk.Combobox(root, values=list(startTags.keys()), font=fontStyle)
+
+#comboExample.grid(column=0, row=1)
+startCombo.config(state='readonly')
+#startCombo.pack()
+startCombo.current(0)
+
+endCombo = ttk.Combobox(root, values=list(endTags.keys()), font=fontStyle)
+
+#comboExample.grid(column=0, row=1)
+endCombo.config(state='readonly')
+#endCombo.pack()
+endCombo.current(0)
+
+root.option_add("*TCombobox*Listbox*Font", fontStyle)
+
+
+
+#set up a grid layout
+tk.Label(root, text="Input sequence:", font=fontStyle).grid(row=0, column=0)
+inp.grid(row=0, column=1, columnspan=4)
+
+tk.Label(root, text="Part type prefix:", height=2, font=fontStyle).grid(row=1, column = 1)
+startCombo.grid(row=1, column=2, sticky=tk.W+tk.E)
+tk.Label(root, text="Part type suffix:", height=2, font=fontStyle).grid(row=2, column=1)
+endCombo.grid(row=2, column=2, sticky=tk.W+tk.E)
+filterChk.grid(row=3, column=1)
+buttonFrame.grid(row=3, column=2, sticky=tk.W+tk.E)
+
+#test.grid(row=1, column=0, columnspan=3)
+
+tk.Label(root, text="Warnings:", font=fontStyle).grid(row=1, column=3, rowspan=3, sticky=tk.N+tk.S)
+warnings.grid(row=1, column=4, rowspan=3, sticky=tk.W+tk.E+tk.N+tk.S)
+
+tk.Label(root, text="Forward primers:", font=fontStyle).grid(row=4, column=0)
+outpFwd.grid(row=4, column=1, columnspan=4)
+tk.Label(root, text="Reverse primers:", font=fontStyle).grid(row=5, column=0)
+outpRev.grid(row=5, column=1, columnspan=4)
+
+#the button frame layout
+genButton.grid(row=0, column=0, sticky=tk.W+tk.E)
+expButton.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+
+root.mainloop()
+
+
---
+++ ShinyApp/app.R
@@ -0,0 +1,281 @@
+library(shiny)
+library(DT)
+library(stringr)
+#install.packages("shinyBS")
+library(shinyBS)
+#install.packages("pracma")
+library(pracma)
+
+#runExample("01_hello")
+
+#there's a bug in textAreaInput, fixed by the code below (which is a copy of the same input)
+#See https://stackoverflow.com/questions/40808889/a-dynamically-resizing-shiny-textareainput-box
+#based on Shiny textAreaInput
+textAreaInput2 <- function (inputId, label, value = "", width = NULL, height = NULL, 
+                            cols = NULL, rows = NULL, placeholder = NULL, resize = NULL) 
+{
+  value <- restoreInput(id = inputId, default = value)
+  if (!is.null(resize)) {
+    resize <- match.arg(resize, c("both", "none", "vertical", 
+                                  "horizontal"))
+  }
+  style <- paste("max-width: 100%;", if (!is.null(width)) 
+    paste0("width: ", validateCssUnit(width), ";"), if (!is.null(height)) 
+      paste0("height: ", validateCssUnit(height), ";"), if (!is.null(resize)) 
+        paste0("resize: ", resize, ";"))
+  if (length(style) == 0) 
+    style <- NULL
+  div(class = "form-group", 
+      tags$label(label, `for` = inputId), tags$textarea(id = inputId, 
+                                                        class = "form-control", placeholder = placeholder, style = style, 
+                                                        rows = rows, cols = cols, value))
+}
+
+#super inefficent, but that doesn't really matter
+cleanupSeq = function(s) {
+  if (str_length(s) == 0) {
+    return(s)
+  }
+  ss = strsplit(s,"")[[1]]
+  ret = ""
+  for (i in 1:length(ss)) { 
+    if ((ss[i] == 'A') | (ss[i] == 'T') | (ss[i] == 'C') | (ss[i] == 'G')) {
+      ret = paste0(ret,ss[i])
+    }
+  }
+  return (ret)
+}
+
+compChar = function(s) {
+  if (s == "A") return ("T")
+  if (s == "T") return ("A")
+  if (s == "C") return ("G")
+  if (s == "G") return ("C")
+  return("X")
+}
+
+
+complementary = function(s) {
+  ss = strsplit(s,"")[[1]]
+  ret = ""
+  for (i in 1:length(ss)) {
+    ret = paste0(ret, compChar(ss[i]))
+  }
+  return (ret)
+}
+
+gcContent = function(s) {
+  return(str_count(s, "[GC]")/str_length(s))
+}
+
+
+#Tm= 64.9 +41*(yG+zC-16.4)/(wA+xT+yG+zC)
+#from: http://insilico.ehu.es/tm.php?formula=basic
+meltTemp = function(s) {
+  wA = str_count(s, "A")
+  xT = str_count(s, "T")
+  yG = str_count(s, "G")
+  zC = str_count(s, "C")
+
+  return (64.9 +41*(yG+zC-16.4)/(wA+xT+yG+zC))
+}
+
+checkPrimer = function(s) {
+  mt = meltTemp(s)
+  return ((mt >= 48) & (mt <= 60))
+}
+
+reverse = function(s) {
+  ss = strsplit(s, "")[[1]]
+  revss <- rev(ss)
+  return (paste(revss, collapse=""))
+}
+
+
+univStartTag = "GCATCGTCTCATCGGTCTCA"
+univEndTag = "TGAGACCTGAGACGGCAT"
+
+startValueNames = c("Type 2", "Type 3", "Type 3A", "Type 3B", "Type 4", "Type 4A", "Type 4B", "Type 6", "Type 7", "Type 8", "Type 8A", "Type 8B")
+startValueVals = c("AACG", "TATG", "TATG", "TTCT", "ATCCTAACTCGAG", "ATCC", "TGGC", "TACA", "GAGT", "CCGAGCGGCCGC", "CCGAGCGGCCGC", "CAAT")
+
+endValueNames = c("Type 2", "Type 3", "Type 3A", "Type 3B", "Type 4", "Type 4A", "Type 4B", "Type 6", "Type 7", "Type 8", "Type 8A", "Type 8B")
+endValueVals = c("AGATCTATG", "GGATCC", "GGTTCT", "GGATCC", "GCTG", "TAACTCGAGTGGC", "GCTG", "GAGT", "CCGA", "GCGGCCGCCCCT", "GCGGCCGCCAAT", "CCCT")
+
+enzymeRecSitesIds = c("BsaI", "BsmBI", "NotI", "BglII", "BamHI", "XhoI")
+enzymeRecSitesSeqs = c("GGTCTC", "CGTCTC", "GCGGCCGC", "AGATCT", "GGATCC", "CTCGAG")
+
+
+#startChoices = startValueVals
+#names(startChoices) = startValueNames
+#endChoices = endValueVals
+#names(endChoices) = endValueNames
+
+
+
+
+ui <- fluidPage(
+  titlePanel("MoClo Design Tool"), 
+  fluidRow(
+    column(12,
+           textAreaInput2("inputSeq", "Input sequence", width="100%", resize="none", cols=140, rows=8)
+    )
+  ),
+  fluidRow(
+    column(6,
+           mainPanel(width="100%",
+                     fluidRow(
+                       column(6, selectInput("startCombo", "Part type prefix", startValueNames,  width = "100%", selectize = F)),
+                       column(6, selectInput("endCombo", "Part type suffix", endValueNames,  width = "100%", selectize = F))
+                     ),
+                     fluidRow(
+                       column(4, checkboxInput("filterCheck", "Filter primers", T)),
+                       column(4, actionButton("genButton", "Gen. primers",  width = "100%")),
+                       column(4,actionButton("expButton", "Export FASTA",  width = "100%"))
+                       #column(4, downloadButton('downloadSeq', 'Download',  width = "100%"))
+                     )
+                     
+           )
+           
+    ),
+    column(6,
+           uiOutput("warningsTitle"),
+           verbatimTextOutput("warnings"),
+           tags$head(tags$style("#warnings{color: red;overflow-y:scroll;max-height: 100px;}"))
+
+    )
+  ),
+  fluidRow(
+    column(12,
+           conditionalPanel(
+             'output.primersGenerated === "t"',
+             tabsetPanel(
+                id = 'outputTab',
+                tabPanel("Forward primer", DT::dataTableOutput("outpFwd", width="100%")),
+                tabPanel("Reverse primer", DT::dataTableOutput("outpRev", width="100%"))
+             )
+           )
+    )
+  ),
+  bsModal("expModalDlg", "Export FASTA", "expButton", size = "large",textInput('filename', 'Filename', "Sequence"), downloadButton('downloadSeq', 'Download'))
+)
+
+primGen = "f"
+
+onGenButton = function(input, output, session) {
+  primGen = "t"
+  output$primersGenerated = renderText(primGen)
+  
+  inp = paste0(input$inputSeq)
+  sq = cleanupSeq(inp)
+  startTag = lookup(input$startCombo, startValueNames, startValueVals)
+  endTag = lookup(input$endCombo, endValueNames, endValueVals)
+
+
+  dfFwd <- data.frame(Sequence=character(),
+                      GCCont=double(), 
+                      Length=integer(), 
+                      MeltTm=double(),
+                      stringsAsFactors=FALSE)   
+  dfRev <- data.frame(Sequence=character(),
+                      GCCont=double(), 
+                      Length=integer(), 
+                      MeltTm=double(),
+                      stringsAsFactors=FALSE)   
+  
+  
+  for (le in 17:28) {
+    forwardPrimer = substr(sq, 1, le)
+    if ((!input$filterCheck) | checkPrimer(forwardPrimer)) {
+      fwdPrimTot = paste0(univStartTag, startTag, forwardPrimer)
+      dfFwd = rbind(dfFwd, c(fwdPrimTot,
+                             round(gcContent(forwardPrimer), 2),
+                             le,
+                             round(meltTemp(forwardPrimer), 1)), stringsAsFactors=F)
+    }
+    
+    reversePrimer = reverse(substr(sq, str_length(sq) - le + 1, str_length(sq))) #reverses automatically
+    if ((!input$filterCheck) | checkPrimer(reversePrimer)) {
+      revPrimTot = paste0(complementary(reverse(univEndTag)), endTag, complementary(reversePrimer)) 
+      dfRev = rbind(dfRev, c(revPrimTot,
+                             round(gcContent(reversePrimer), 2),
+                             le,
+                             round(meltTemp(reversePrimer), 1)), stringsAsFactors=F)
+    }
+  }
+  print(forwardPrimer)
+  print(reversePrimer)
+  
+  colnames(dfFwd) = c("Primer sequence", "GC", "Len", "Melt Tm")
+  colnames(dfRev) = c("Primer sequence", "GC", "Len", "Melt Tm")
+  
+  output$outpFwd = DT::renderDataTable(DT::datatable({dfFwd}, width="100%", rownames = FALSE, class="compact", selection = 'none', options = list(dom = 't', pageLength = 20)))#, autoWidth = TRUE,
+  output$outpRev = DT::renderDataTable(DT::datatable({dfRev}, width="100%", rownames = FALSE, class="compact", selection = 'none', options = list(dom = 't', pageLength = 20)))#, autoWidth = TRUE,
+
+  #Warnings:
+  strWarn = ""
+  for (i in 1:length(enzymeRecSitesIds)) {
+    ind = strfind(sq, enzymeRecSitesSeqs[i])
+    print(ind)
+    for (a in ind) {
+      strWarn = paste0(strWarn, "Restr. enzyme recogn. site found in seq: ", enzymeRecSitesIds[i], ": ", enzymeRecSitesSeqs[i], " at pos ", a, "\n")
+    }
+  }
+  #issue warnings for certain parts, since the start/end codon is included in the primer
+  if (input$startCombo == "Type 3" | input$startCombo == "Type 3A") {
+    strWarn = paste0(strWarn,
+                    "Start codon is already included in fwd primer. Do not include stop codon in pasted sequence if part will be used as fusion or tagged protein.\n")
+  }
+  if (input$endCombo == "Type 3B") {
+    strWarn = paste0(strWarn,
+                    "Stop codon is already included in rev primer. Do not include stop codon in pasted sequence if part will be used as fusion or tagged protein.\n")
+  }
+#Test scrollbar
+#  for (i in 1:10) {
+#    strWarn = paste0(strWarn, "Warning: Test\n")
+#  }
+  
+  #updateTextAreaInput(session, "warnings", value = strWarn)
+  output$warningsTitle = renderText({HTML("<b>Warnings</b>")})
+  output$warnings = renderText(strWarn)
+  
+}
+
+
+
+server <- function(input, output, session) {
+  output$primersGenerated = renderText(primGen)
+  outputOptions(output, "primersGenerated", suspendWhenHidden = FALSE)
+  observeEvent(input$genButton, {
+    onGenButton(input, output, session)
+  })
+
+  output$downloadSeq <- downloadHandler(
+    filename = paste0(input$filename, ".FASTA"),
+    #filename = paste0("test", ".FASTA"),
+    content = function(file) {
+      stringsToWrite = paste0(">Yeast toolkit export. Name: ", input$filename, " Starts at ", input$startCombo, ", ends at ", input$endCombo)
+      
+      #generate sequence
+      fwdOverhangStd = "TCGGTCTCA"
+      fwdOverhangTypeSpec = input$startCombo
+      seq = cleanupSeq(input$inputSeq)
+      revOverhangStd = "TGAGACC"
+      revOverhangTypeSpec = input$endCombo
+      backbone = "AGACCAATAAAAAACGCCCGGCGGCAACCGAGCGTTCTGAACAAATCCAGATGGAGTTCTGAGGTCATTACTGGATCTATCAACAGGAGTCCAAGCGAGCTCGATATCAAATTACGCCCCGCCCTGCCACTCATCGCAGTACTGTTGTAATTCATTAAGCATTCTGCCGACATGGAAGCCATCACAAACGGCATGATGAACCTGAATCGCCAGCGGCATCAGCACCTTGTCGCCTTGCGTATAATATTTGCCCATGGTGAAAACGGGGGCGAAGAAGTTGTCCATATTGGCCACGTTTAAATCAAAACTGGTGAAACTCACCCAGGGATTGGCTGAAACGAAAAACATATTCTCAATAAACCCTTTAGGGAAATAGGCCAGGTTTTCACCGTAACACGCCACATCTTGCGAATATATGTGTAGAAACTGCCGGAAATCGTCGTGGTATTCACTCCAGAGCGATGAAAACGTTTCAGTTTGCTCATGGAAAACGGTGTAACAAGGGTGAACACTATCCCATATCACCAGCTCACCGTCTTTCATTGCCATACGAAATTCCGGATGAGCATTCATCAGGCGGGCAAGAATGTGAATAAAGGCCGGATAAAACTTGTGCTTATTTTTCTTTACGGTCTTTAAAAAGGCCGTAATATCCAGCTGAACGGTCTGGTTATAGGTACATTGAGCAACTGACTGAAATGCCTCAAAATGTTCTTTACGATGCCATTGGGATATATCAACGGTGGTATATCCAGTGATTTTTTTCTCCATTTTAGCTTCCTTAGCTCCTGAAAATCTCGATAACTCAAAAAATACGCCCGGTAGTGATCTTATTTCATTATGGTGAAAGTTGGAACCTCTTACGTGCCCGATCAATCATGACCAAAATCCCTTAACGTGAGTTTTCGTTCCACTGAGCGTCAGACCCCGTAGAAAAGATCAAAGGATCTTCTTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCTTGCAAACAAAAAAACCACCGCTACCAGCGGTGGTTTGTTTGCCGGATCAAGAGCTACCAACTCTTTTTCCGAAGGTAACTGGCTTCAGCAGAGCGCAGATACCAAATACTGTTCTTCTAGTGTAGCCGTAGTTAGGCCACCACTTCAAGAACTCTGTAGCACCGCCTACATACCTCGCTCTGCTAATCCTGTTACCAGTGGCTGCTGCCAGTGGCGATAAGTCGTGTCTTACCGGGTTGGACTCAAGACGATAGTTACCGGATAAGGCGCAGCGGTCGGGCTGAACGGGGGGTTCGTGCACACAGCCCAGCTTGGAGCGAACGACCTACACCGAACTGAGATACCTACAGCGTGAGCTATGAGAAAGCGCCACGCTTCCCGAAGGGAGAAAGGCGGACAGGTATCCGGTAAGCGGCAGGGTCGGAACAGGAGAGCGCACGAGGGAGCTTCCAGGGGGAAACGCCTGGTATCTTTATAGTCCTGTCGGGTTTCGCCACCTCTGACTTGAGCGTCGATTTTTGTGATGCTCGTCAGGGGGGCGGAGCCTATGGAAAAACGCCAGCAACGCGGCCTTTTTACGGTTCCTGGCCTTTTGCTGGCCTTTTGCTCACATGTTCTTTCCTGCGTTATCCCCTGATTCTGTGGATAACCGTAG"
+      
+      totSeqToExp = paste0(fwdOverhangStd, fwdOverhangTypeSpec, seq, revOverhangTypeSpec, revOverhangStd, backbone)
+
+      
+      ll = str_length(totSeqToExp)
+      for (i in seq(1, ll, by=80)) {
+        stringsToWrite = c(stringsToWrite,paste0(substr(totSeqToExp, i, i+79)))
+      }
+      writeLines(stringsToWrite, con=file)
+      toggleModal(session, modalId = "expModalDlg") #close the dialog
+    }) 
+}
+
+
+
+#don't add any code after the shinyApp line below, it has to be the last line!
+shinyApp(ui = ui, server = server)
